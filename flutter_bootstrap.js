fa67013b53da4409d77d68e467ea1ad8135c2a8d
CineFile Deploy <deploy@cinefile.local>
2026-07-10 18:10:24 +0300

    Revert logo and splash screen

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2623020790" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1505904724" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
